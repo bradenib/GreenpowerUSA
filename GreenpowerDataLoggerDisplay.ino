3f980ad59d319f49f3b45daed5a6a9435d1550f7
bradenib <braden@iblamar.org>
2018-04-09 08:23:22 -0500

    Major changes, worked for race
    Displays speed, inefficiency, and voltage.
    Records EVERYTHING - time, time interval, revolution, raw current, voltage, raw current times time interval, inefficiency
    Most calculating is done with the wheel rotation interrupt, so more data.

--- GreenpowerDataLoggerDisplay.ino
+++ GreenpowerDataLoggerDisplay.ino
@@ -28,99 +28,77 @@ LiquidCrystal lcd(8, 7, 5, 4, 3, 6);  // the last pin had a default (sample prog
 const int chipSelect = 10;
 unsigned long trigger_time=0;
 unsigned long last_trigger_time=0;
-int time_interval;
+int time_interval=3000;  //anything large, so that it starts with a speed of zero
 float speedo=0;
 float voltage;
-float current;
-int rawCurrent=0;
-int currentZero;
-int triggerLED=9;
-int writeLED=8;
+int current;
+int currentZero = 517;
 int currentPIN=A2;
 int voltagePIN=A1;
 byte places1=1;
 byte places0=0;
+long int rev=0;
+float charge=0.0;
+float eff=0.0;
+const int numReadings = 100;
+long int readings[numReadings];
+int readIndex=0;
+long int total = 0;
 
 void setup() {
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading]=0;
+  }
   //initialize LCD
   lcd.begin(16, 2);
-
-  //welcome driver
-  lcd.print("Welcome to car");
-  lcd.setCursor(0,1);
-  lcd.print("123.");
-  delay(1000);
-  lcd.clear();
-  
   //Zero the current sensor
-  lcd.print("Zeroing the");
-  lcd.setCursor(0,1);
-  lcd.print("current sensor");
-  delay(500);
-  currentZero=analogRead(currentPIN);
-  lcd.clear();
-  delay(500);
-    
-  //Initialize pin functions
-  pinMode(writeLED, OUTPUT);
-  pinMode(triggerLED, OUTPUT);
-
+  //currentZero=analogRead(currentPIN);
+  
   //attach interrupt so that every time the magnet passes the sensor the time_period function will update the period of rotation (time_interval)
   attachInterrupt(0,time_period,FALLING);  //attach speed sensor to digital pin 2
-
+  
   // see if the card is present and can be initialized, and communicate SD status to LCD
   if (!SD.begin(chipSelect)) {
     lcd.print("Card failed!");
   }
-  else {
-    lcd.print("Card good.");
-  }
   delay(2000);
   lcd.clear();
-
-  lcd.print("Ready to roll.");
-  delay(1000);
-  lcd.clear();
-
-
-  
 }
 
 void loop() {
-  //debugging LEDs for checking if SD is writting and if the magnetic sensor is triggering for speed
-  digitalWrite(writeLED,LOW);
-  digitalWrite(triggerLED,LOW);
-
   //refresh rate for the speed display
   delay(200); 
 
-  //a timeout in case there hasn't been an interrupt (wheel turn) in a long time
-  if (millis()-last_trigger_time>2000){  
+  //measuring speed from time interval and wheel circ.
+  if (millis()-last_trigger_time>2000){  //set minimum speed to show zero
     speedo=0;
+  } else{
+    speedo=1000*1.57/time_interval;
   }
 
-  //measuring analog sensors and converting
-  speedo=1000*1.25/time_interval;
-  rawCurrent=analogRead(currentPIN);
-  current=(float(analogRead(currentPIN)-currentZero))/1023*5/0.0098;  //difference from zero converted to voltage, then current with 40mV per amp, according to manufacturer
-  if (current<0.7){
-    current=0.00;
-  }
+  //measuring and calculating voltage
   voltage=float(analogRead(voltagePIN))/1023*5*6.1;  // conversion from 10 bit to measured voltage (0-5V), then up again due to voltage divider circuit
 
   //sending readings to LCD
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("Spd  Pwr  Vlt");
+  if (millis()>1500000){    //timer for driver change notification
+    lcd.print("Head to the pit!");
+  }
+  else{
+    lcd.print("spd  eff  vlt");
+  }
   lcd.setCursor(0,1);
-  lcd.print(time_interval);
+  lcd.print(speedo,1);
   lcd.setCursor(5,1);
-  lcd.print(current*voltage,places0);
+  lcd.print(eff,0);
   lcd.setCursor(10,1);
-  lcd.print(voltage,places1);
+  lcd.print(voltage);
+
+  
 
   // make a string for assembling the data to log:
-  String dataString = String(millis()/1000)+","+String(speedo)+","+String(voltage)+","+String(current);
+  String dataString = String(millis())+","+String(rev)+","+String(time_interval)+","+String(speedo)+","+String(voltage)+","+String(current)+","+String(charge)+","+String(eff);
   
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
@@ -129,7 +107,6 @@ void loop() {
   // if the file is available, write to it:
   if (dataFile) {
     dataFile.println(dataString);
-    digitalWrite(writeLED,HIGH);
     lcd.setCursor(15,1);
     lcd.blink();
     dataFile.close();
@@ -141,12 +118,29 @@ void loop() {
   }
 }
 
-int time_period(){
+void time_period(){
   trigger_time=millis();
-  if (trigger_time-last_trigger_time>10){
+  if (trigger_time-last_trigger_time>100){            //ignores short time intervals caused by noise triggering the interrupt
     time_interval=trigger_time-last_trigger_time;
     last_trigger_time=trigger_time;
+    rev=rev+1;  //adding up the cumulative distance traveled in revolutions
+    current=analogRead(currentPIN)-currentZero;  //difference from zero converted to voltage, then current with 40mV per amp, according to manufacturer
+    if (current<2){
+      current=0;
+    }
+    
+    charge=charge+float((current*time_interval))/1000;
+    total=total-readings[readIndex];
+    readings[readIndex]=time_interval*current/100;
+    total=total+readings[readIndex];
+    readIndex=readIndex+1;
+    if (readIndex>=numReadings){
+      readIndex=0;
+    }
+    eff=float(total)/float(numReadings);
+   
   }
+
 }
 
 
